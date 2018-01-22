456e976a60056078168be20c93baf7d6160a0392
kibrey101 <kibtsegai@gmail.com>
2018-01-22 15:28:12 +0200

    a

---
+++ TopHundred/Entities/IcoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopHundred.Entities
+{
+    public class IcoItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
