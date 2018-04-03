a44dab5cff3b6431180130687f09562082b128c4
kibrey101 <kibtsegai@gmail.com>
2018-04-03 10:51:57 +0300

    aa

---
+++ TopHundred/Migrations/20180403071422_NewCols.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TopHundred.Migrations
+{
+    public partial class NewCols : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "IcoItems",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PriceCurrency",
+                table: "IcoItems",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "IcoItems");
+
+            migrationBuilder.DropColumn(
+                name: "PriceCurrency",
+                table: "IcoItems");
+        }
+    }
+}
